cf094a8369adb30062238d9a35860ea7bfab3d12
tsuryanto <tsuryanto16@gmail.com>
2022-02-06 17:14:00 +0700

    create migration and model for camp, camp benefit, checkout

---
+++ database/migrations/2022_02_06_094315_create_camps_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CreateCampsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camps', function (Blueprint $table) {
+            $table->id();
+            $table->text('title', 100);
+            $table->text('slug');
+            $table->integer('price')->unsigned();            
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camps');
+    }
+}
---
+++ database/migrations/2022_02_06_095225_create_checkouts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CreateCheckoutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->constrained("users");
+            $table->foreignId("camp_id")->constrained("camps");
+            $table->text("card_number", 20);
+            $table->date("expired");
+            $table->text("cvc", 3);
+            $table->boolean("is_paid")->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkouts');
+    }
+}
