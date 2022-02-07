10e0c7d2813f13bad85ed30c972210d9100f4b14
tsuryanto <tsuryanto16@gmail.com>
2022-02-07 18:02:31 +0700

    Install Socialite and integrate it

--- routes/auth.php
+++ routes/auth.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
@@ -17,16 +18,17 @@ Route::get('/register', [RegisteredUserController::class, 'create'])
 Route::post('/register', [RegisteredUserController::class, 'store'])
                 ->middleware('guest');
 
-Route::get('/login', function(){
-    return view('login');
-})->middleware('guest')->name('login');
-
-Route::get('/login/admin', [AuthenticatedSessionController::class, 'create'])
+Route::get('/login', [UserController::class, 'login'])
                 ->middleware('guest')
                 ->name('login');
 
+Route::get('/login/admin', [AuthenticatedSessionController::class, 'create'])
+                ->middleware('guest')
+                ->name('login.admin');
+
 Route::post('/login/admin', [AuthenticatedSessionController::class, 'store'])
-                ->middleware('guest');
+                ->middleware('guest')
+                ->name('login.admin');
 
 Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->middleware('guest')
